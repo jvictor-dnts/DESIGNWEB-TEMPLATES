c13644c0a473de575bd497d1ef73b81b26f1d691
Lucas Eduardo <lucaseduardosilva0848@gmail.com>
2025-07-31 21:58:54 -0300

    adicionando transição ao trocar de tema, e mudando a cor do titulo dos cards no manage

--- templates/dashboard/script.js
+++ templates/dashboard/script.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.change-theme-button')
       .addEventListener('click', function () {
+        document.body.classList.add('theme-transition');
         document.body.classList.toggle('dark-theme');
+        setTimeout(() => {
+            document.body.classList.remove('theme-transition');
+        }, 400);
     });
 });
